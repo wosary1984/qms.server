db34d52147cf9e3f461219d91949e162e94f8601
bruce wang <bruce.wang01@sap.com>
2019-06-24 13:36:15 +0800

    remove unused import

--- src/main/java/qms/controller/session/SessionController.java
+++ src/main/java/qms/controller/session/SessionController.java
@@ -2,19 +2,14 @@ package qms.controller.session;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import qms.controller.BaseController;
