c571e3e4b3acd0a7e91d85f64a88154136b7a453
bruce wang <bruce.wang01@sap.com>
2019-09-17 14:26:29 +0800

    add sub page

--- src/main/java/qms/repository/portal/SamplePages.java
+++ src/main/java/qms/repository/portal/SamplePages.java
@@ -11,8 +11,6 @@ import qms.repository.person.PersonPrivilege;
 @Component
 public class SamplePages {
 
-    
-
     private interface ADMIN_PAGE {
         final String name = "Admin";
         final String path = "/admin";
@@ -44,8 +42,6 @@ public class SamplePages {
         final String privilege = PersonPrivilege.write;
     }
 
-    
-
     @Autowired
     private ApplicationPageRepository applicationPageRepository;
 
@@ -56,6 +52,7 @@ public class SamplePages {
         final boolean hasChild = false;
         final boolean hasDeleted = false;
     }
+
     private void setHome() {
         // HOME
         if (applicationPageRepository.findByPagename(HOME_PAGE.name).size() == 0) {
@@ -122,13 +119,14 @@ public class SamplePages {
     private interface SEMI_WAFER_PAGE {
         final String name = "Wafer";
         final String path = SEMI_PAGE.path + "/wafer";
-        //final String icon = "fa fa-braille";
+        // final String icon = "fa fa-braille";
         final String icon = "";
         final String parent = SEMI_PAGE.name;
         // final String privilege = PersonPrivilege.write;
         final boolean hasChild = false;
         final boolean hasDeleted = false;
     }
+
     private void setSemi() {
         // SEMI
         if (applicationPageRepository.findByPagename(SEMI_PAGE.name).size() == 0) {
@@ -155,9 +153,55 @@ public class SamplePages {
         }
     }
 
+
+    private interface DATA_PAGE {
+        final String name = "Data";
+        final String path = "/data";
+        final String icon = "fa fa-database";
+        final boolean hasChild = true;
+        final boolean hasDeleted = false;
+    }
+
+    private interface DATA_MAIN_EVENT_PAGE {
+        final String name = "Event";
+        final String path = SEMI_PAGE.path + "/event";
+        // final String icon = "fa fa-braille";
+        final String icon = "";
+        final String parent = DATA_PAGE.name;
+        // final String privilege = PersonPrivilege.write;
+        final boolean hasChild = false;
+        final boolean hasDeleted = false;
+    }
+    private void setData() {
+        // SEMI
+        if (applicationPageRepository.findByPagename(DATA_PAGE.name).size() == 0) {
+            ApplicationPage data = new ApplicationPage();
+            ApplicationPage event = new ApplicationPage();
+            data.setPagename(DATA_PAGE.name);
+            data.setHasDeleted(DATA_PAGE.hasDeleted);
+            data.setPath(DATA_PAGE.path);
+            data.setIcon(DATA_PAGE.icon);
+            data.setHasChild(DATA_PAGE.hasChild);
+            // applicationPageRepository.save(semi);
+            if (applicationPageRepository.findByPagename(DATA_MAIN_EVENT_PAGE.name).size() == 0) {
+                event.setPagename(DATA_MAIN_EVENT_PAGE.name);
+                event.setHasDeleted(DATA_MAIN_EVENT_PAGE.hasDeleted);
+                event.setHasChild(DATA_MAIN_EVENT_PAGE.hasChild);
+                event.setIcon(DATA_MAIN_EVENT_PAGE.icon);
+                event.setPath(DATA_MAIN_EVENT_PAGE.path);
+                // waferPerson.setPrivilege(SEMI_WAFER_PAGE.privilege);
+                event.setParentPageName(DATA_MAIN_EVENT_PAGE.parent);
+
+                data.setChilds(Arrays.asList(event));
+            }
+            applicationPageRepository.save(data);
+        }
+    }
+
     public void init() {
         this.setHome();
         this.setAdmin();
         this.setSemi();
+        this.setData();
     }
 }
