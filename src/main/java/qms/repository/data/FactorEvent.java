7b024adcf90c4ae1f3a66ebc0c365dcc0d9965f4
bruce wang <bruce.wang01@sap.com>
2019-10-17 17:42:38 +0800

    123

--- src/main/java/qms/repository/data/FactorEvent.java
+++ src/main/java/qms/repository/data/FactorEvent.java
@@ -1,5 +1,6 @@
 package qms.repository.data;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -113,8 +114,16 @@ public class FactorEvent extends BaseEntity {
         this.day = day;
     }
 
-    public Date getDate() {
-        return date;
+    public String getDate() {
+        if (this.bc)
+            return null;
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.date);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return Integer.toString(year) + "/" + Integer.toString(month) + "/" + Integer.toString(day);
     }
 
     public void setDate(Date date) {
